74a3c2ff4d9aa3b6c49b83098ea9327ed40517c6
Evgen <evgenprogrammer@gmail.com>
2020-11-28 15:42:44 +0300

    написал сервер

--- main.go
+++ main.go
@@ -1,1 +1,85 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"github.com/EvgenyiK/gChat/proto"
+	"google.golang.org/grpc"
+)
+
+
+type Connection struct{
+	stream proto.Broadcast_CreateStreamServer
+	id string
+	active bool
+	err chan error
+}
+
+type Server struct{
+	Connection []*Connection
+}
+
+
+//CreateStream Создание соединения и добавление его в список соединений
+func (s *Server) CreateStream(pconn *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
+	conn:= &Connection{
+		stream: stream,
+		id: pconn.User.Id,
+		active: true,
+		err: make(chan error),
+	}
+
+	s.Connection = append(s.Connection, conn)
+	return <-conn.err
+}
+
+//BroadcastMessage  отправляет сообщения всем активным пользователям
+func (s *Server) BroadcastMessage(ctx context.Context, msg *proto.Message) (*proto.Close, error) {
+	wait:= &sync.WaitGroup{}
+	done:= make(chan int)
+
+	for _, conn := range s.Connection {
+		log.Println(conn.id)
+		wait.Add(1)
+
+		go func(msg *proto.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err:= conn.stream.Send(msg)
+				log.Printf("Sending message %v to user %v", msg.Id, conn.id)
+				if err != nil {
+					log.Printf("Error with stream %v. Error: %v",  conn.stream, err)
+				}
+				conn.active = false
+				conn.err <- err
+			}
+		}(msg, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+	return &proto.Close{}, nil
+}
+
+func main()  {
+	var connections []*Connection
+	server:= &Server{connections}
+	grpcServer:= grpc.NewServer()
+	listener,err:= net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("error creating the server %v", err)
+	}
+	fmt.Println("Starting server at port:8080")
+
+	proto.RegisterBroadcastServer(grpcServer, server)
+	grpcServer.Serve(listener)
+}
